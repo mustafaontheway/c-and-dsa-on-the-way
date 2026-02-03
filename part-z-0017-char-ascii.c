197cf66050f2e3f0ff06b7084ad81a189572de3a
Mustafa Buyukdereli <77589867+mustafaontheway@users.noreply.github.com>
2026-02-03 11:02:51 +0300

    Create part-z-0017-char-ascii.c

---
+++ part-z-0017-char-ascii.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+// https://www.ascii-code.com/
+
+int main() {
+
+    char my_char = 'D';
+
+    printf("ASCII value: %d\n", my_char); // ASCII value: 68
+
+    char ascii_val = 68;
+
+    printf("My char: %c", ascii_val);
+
+    return 0;
+}
+
+// ASCII value: 68
+// My char: D
